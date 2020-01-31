cb9eb1006612b457ce56cf49031548389943471d
Chiming Chang <Arvim Chang>
2020-01-31 23:43:40 +0800

    Try footer background

--- resources/views/frontend/layouts/banner.blade.php
+++ resources/views/frontend/layouts/banner.blade.php
@@ -28,11 +28,11 @@
 						<nav class="menu menu--horatio link-effect-8" id="link-effect-8">
 							<ul class="w3layouts_menu__list">
 								<li class="@yield('menu_home')"><a href="{{ route('home') }}">Home</a></li>
+{{--
 								<li class="@yield('menu_about')"><a href="{{ route('about') }}">About Us</a></li> 
 								<li class="@yield('menu_services')"><a href="{{ route('services') }}">Services</a></li>
 								<li class="@yield('menu_gallery')"><a href="{{ route('gallery') }}">Gallery</a></li> 
 								<li class="@yield('menu_contact')"><a href="{{ route('contact') }}">Contact Us</a></li>
-{{--
 --}}
 							</ul>
 						</nav>
