3ade54d8ad1cb965c6ab958285ce2cbfee0f363a
Chiming Chang <Arvim Chang>
2020-02-14 00:29:53 +0800

    Try email verify

--- routes/web.php
+++ routes/web.php
@@ -12,7 +12,7 @@
 */
 
 Route::get('/', 'Frontend\HomeController@index')->name('home');
-Route::get('/about', 'Frontend\AboutController@index')->name('about')->middleware('isadmin');
+Route::get('/about', 'Frontend\AboutController@index')->name('about');
 
 Auth::routes(['verify' => true]);
 
