d5abc79c96105db46c92056898d58de9926769dd
Chiming Chang <Arvim Chang>
2020-02-16 00:32:23 +0800

    Remove HOME from auth views.

--- resources/views/frontend/layouts/breadcrumbs.blade.php
+++ resources/views/frontend/layouts/breadcrumbs.blade.php
@@ -1,25 +1,30 @@
 <!-- breadcrumbs -->
 <div class="breadcrumbs">
     <div class="container">
-        <div class="w3layouts_breadcrumbs_left">
-            <ul>
-                <li><i class="fa fa-home" aria-hidden="true"></i><a href="{{ route('home') }}">首頁 Home</a><span>/</span></li>
-                <li><i class="{{ $icon }}" aria-hidden="true"></i>{{ $title }}</li>
-            </ul>
+        <div class="row justify-content-center">
+            <div class="col-md-4">
+                <div class="w3layouts_breadcrumbs_left">
+                    <ul>
+                        <li><i class="{{ $icon }}" aria-hidden="true"></i>{{ $title }}</li>
+                    </ul>
+                </div>
+            </div>
+            <div class="col-md-4">
+                <div class="w3layouts_breadcrumbs_right">
+                    <ul>
+                        @guest
+                        @if( $type == 'Login' )
+                        <li><i class="fa fa-registered" aria-hidden="true"></i><a href="{{ route('register') }}">註冊 Register</a></li>
+                        @endif
+                        @if( $type == 'Register' )
+                        <li><i class="fa fa-sign-in" aria-hidden="true"></i><a href="{{ route('login') }}">登入 Login</a></li>
+                        @endif
+                        @endguest
+                    </ul>
+                </div>
+            </div>
+            <div class="clearfix"> </div>
         </div>
-        <div class="w3layouts_breadcrumbs_right">
-            <ul>
-                @guest
-                @if( $type == 'Login' )
-                <li><i class="fa fa-registered" aria-hidden="true"></i><a href="{{ route('register') }}">註冊 Register</a></li>
-                @endif
-                @if( $type == 'Register' )
-                <li><i class="fa fa-sign-in" aria-hidden="true"></i><a href="{{ route('login') }}">登入 Login</a></li>
-                @endif
-                @endguest
-            </ul>
-        </div>
-        <div class="clearfix"> </div>
     </div>
 </div>
 <!-- //breadcrumbs -->
