370c98231fecebf177a5998b7d7d3544abe3b023
Chiming Chang <Arvim Chang>
2020-02-19 00:34:41 +0800

    Try facebook open graph #3

--- resources/views/frontend/layouts/master.blade.php
+++ resources/views/frontend/layouts/master.blade.php
@@ -19,9 +19,9 @@ Smartphone Compatible web template, free webdesigns for Nokia, Samsung, LG, Sony
 <meta property="og:type" content="movie" />
 <meta property="og:title" content="台南農場 Tainan Farm" />
 <meta property="og:description" content="我們致力於推廣台南的農作物與特產品，希望能讓大家品嘗到來自南部的香甜美味。We are committed to promoting the crops and special products of Tainan, hoping that everyone can enjoy the sweet and delicious taste from the south."/>
-<meta property="og:image" content="https://storage.cloud.google.com/tnfarm.appspot.com/opengraph/banner1.jpg" />
-<meta property="og:image" content="https://storage.cloud.google.com/tnfarm.appspot.com/opengraph/banner2.jpg" />
-<meta property="og:image" content="https://storage.cloud.google.com/tnfarm.appspot.com/opengraph/banner3.jpg" />
+<meta property="og:image" content="https://www.tnfarm.tw/frontend/images/banner1.jpg" />
+<meta property="og:image" content="https://www.tnfarm.tw/frontend/images/banner2.jpg" />
+<meta property="og:image" content="https://www.tnfarm.tw/frontend/images/banner3.jpg" />
 <meta property="og:image:type" content="image/jpg" />
 <meta property="og:image:width" content="640" />
 <meta property="og:image:height" content="427" />
