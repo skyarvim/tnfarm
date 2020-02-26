2726cf4281661b3f1fac4bc781d4b6db06ee692c
Chiming Chang <Arvim Chang>
2020-02-27 00:21:36 +0800

    Recover two images and try user info column #1

--- resources/views/frontend/user/index.blade.php
+++ resources/views/frontend/user/index.blade.php
@@ -9,43 +9,47 @@
 <div class="container">
     <div class="row mb-4"></div>
     <div class="row text-center">
-        <div class="col-xs-3 col-sm-3 col-md-3 mb-4">
-            <div class="card shadow">
-                <div class="card-body">
-                    <i class="fa fa-id-card-o fa-5x mb-3"></i>
-                    <h4>基本資料</h4>
-                    <h5>Profile</h5>
-                    <a href="{{ route('user.profile.show', Auth::user()->id) }}" class="stretched-link"></a>
+        <div class="col-xs-4 col-sm-8 col-md-12 mb-4">
+            <div class="row">
+                <div class="col-xs-3 col-sm-3 col-md-3 mb-4">
+                    <div class="card shadow">
+                        <div class="card-body">
+                            <i class="fa fa-id-card-o fa-5x mb-3"></i>
+                            <h4>基本資料</h4>
+                            <h5>Profile</h5>
+                            <a href="{{ route('user.profile.show', Auth::user()->id) }}" class="stretched-link"></a>
+                        </div>
+                    </div>
                 </div>
-            </div>
-        </div>
-        <div class="col-xs-3 col-sm-3 col-md-3 mb-4">
-            <div class="card shadow">
-                <div class="card-body">
-                    <i class="fa fa-th fa-5x mb-3"></i>
-                    <h4>商品設定</h4>
-                    <h5>Products</h5>
-                    <a href="#" class="stretched-link"></a>
+                <div class="col-xs-3 col-sm-3 col-md-3 mb-4">
+                    <div class="card shadow">
+                        <div class="card-body">
+                            <i class="fa fa-th fa-5x mb-3"></i>
+                            <h4>商品設定</h4>
+                            <h5>Products</h5>
+                            <a href="#" class="stretched-link"></a>
+                        </div>
+                    </div>
                 </div>
-            </div>
-        </div>
-        <div class="col-xs-3 col-sm-3 col-md-3 mb-4">
-            <div class="card shadow">
-                <div class="card-body">
-                    <i class="fa fa-truck fa-5x mb-3"></i>
-                    <h4>出貨管理</h4>
-                    <h4>Shipping</h4>
-                    <a href="#" class="stretched-link"></a>
+                <div class="col-xs-3 col-sm-3 col-md-3 mb-4">
+                    <div class="card shadow">
+                        <div class="card-body">
+                            <i class="fa fa-truck fa-5x mb-3"></i>
+                            <h4>出貨管理</h4>
+                            <h4>Shipping</h4>
+                            <a href="#" class="stretched-link"></a>
+                        </div>
+                    </div>
                 </div>
-            </div>
-        </div>
-        <div class="col-xs-3 col-sm-3 col-md-3 mb-4">
-            <div class="card shadow">
-                <div class="card-body">
-                    <i class="fa fa-list-alt fa-5x mb-3"></i>
-                    <h4>訂單查詢</h4>
-                    <h4>Orders</h4>
-                    <a href="#" class="stretched-link"></a>
+                <div class="col-xs-3 col-sm-3 col-md-3 mb-4">
+                    <div class="card shadow">
+                        <div class="card-body">
+                            <i class="fa fa-list-alt fa-5x mb-3"></i>
+                            <h4>訂單查詢</h4>
+                            <h4>Orders</h4>
+                            <a href="#" class="stretched-link"></a>
+                        </div>
+                    </div>
                 </div>
             </div>
         </div>
